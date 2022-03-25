38fb8cb8f5b8339862e734c0a9f4e71077421be0
Phuong Vi Dang <phuongvi1512@gmail.com>
2022-03-25 13:07:53 -0700

    part 3 using function to look for student avg grade and students with high avg

---
+++ src/app.js
@@ -0,0 +1,31 @@
+// part 2
+function addElementQuery() {
+    const selectElement = document.querySelector(".two")
+    return selectElement
+}
+console.log(addElementQuery())
+
+// part 3
+const students = [
+    { name: "Tasha", grades: [ 80, 67, 75, 62 ] },
+    { name: "Tuvok", grades: [ 90, 88, 91, 92 ] },
+    { name: "Dax", grades: [ 88, 90, 89, 85 ] },
+    { name: "Neelix", grades: [ 60, 54, 48, 50 ] }
+  ]
+  
+const studentsScholarship = []
+  
+for (const student of students) {
+    function findAverage() {
+        var avg = student.grades.reduce((x,y) => x +y, 0) / student.grades.length;
+        return avg
+    }
+
+    let average = findAverage()
+    if(average >= 80){
+        const goodStudent = {name: student.name, gradeAvg: average}
+        studentsScholarship.push(goodStudent)
+        }
+}
+
+console.log(studentsScholarship)
